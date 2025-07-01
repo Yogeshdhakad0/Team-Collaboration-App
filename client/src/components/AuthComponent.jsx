82ba22db45bffd7e9d68cae978b3bc75dd7ce18a
yogeshdhakad0 <kalking121213@gmail.com>
2025-07-01 22:21:06 +0530

    url

--- client/src/components/AuthComponent.jsx
+++ client/src/components/AuthComponent.jsx
@@ -5,29 +5,15 @@ import axios from 'axios';
 const AuthComponent = () => {
   const { user, isAuthenticated } = useAuth0();
 
-//   useEffect(() => {
-//     if (isAuthenticated && user) {
-//    const user = {
-//       name: user.name,
-//       email: user.email,
-// // user.eamil7
-// //       isadmin: true
-//     };
-
-
-//       localStorage.setItem("user", JSON.stringify(user));
-//     }
-//   }, [isAuthenticated, user]);
-
-
-
   useEffect(() => {
+    const   BaseUrl='https://workspace-p7ko.onrender.com'
+
     const fetchUserFromBackend = async () => {
         console.log('sdfdfdsfsdfs')
       if (isAuthenticated && user?.email) {
         console.log(user)
         try {
-          const res = await axios.post("/api/auth/auth0/login", {
+          const res = await axios.post(`${BaseUrl}/api/auth/auth0/login`, {
             email: user.email,
           });
         //   setFullUser(res.data); // your full user data
