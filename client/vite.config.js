66413548db1a0cbff0030054e856dbe5380b98a7
yogeshdhakad0 <kalking121213@gmail.com>
2025-07-02 12:34:38 +0530

    update the target at vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     server:{
     proxy:{
       '/api':{
-        target:'http://localhost:5173/',
+        target:'https://workspace-p7ko.onrender.com',
         changeOrigin:true,
         secure:false
       }
